0b127b05930c26eca63fdcdb6aadf2fa7e88be11
shfana <wambitafana@gmail.com>
2024-06-01 05:40:25 +0300

    Fix: Enable user to input file of their name choosing

--- programfiles/openfile.go
+++ programfiles/openfile.go
@@ -9,49 +9,41 @@ import (
 
 // Function to open input file, create output file, read input file and store contents as a string
 func OpenFile(inputfilePath string, outputfilePath string) (string, error) {
-	// possible errors: broken file, wrong extension, empty, doesn't exist
-	// store fileinfo to use to check stat about the file
-	fileinfo, err := os.Stat(inputfilePath)
-	if os.IsNotExist(err) {
-		return "", fmt.Errorf("file does not exist, ensure sample.txt exists and is not empty")
+	// input file validation
+	fileInfo, err := os.Stat(inputfilePath)
+	if err != nil {
+		return "", fmt.Errorf("error opening input file: %v", err)
 	}
-	// check on file extensions to be .txt only
+
+	if fileInfo.Size() == 0 {
+		return "", fmt.Errorf("error: file exists but is empty")
+	}
+
 	if filepath.Ext(inputfilePath) != ".txt" {
-		return "", fmt.Errorf("wrong input file extension use .txt files only")
+		return "", fmt.Errorf("wrong file extension, use .txt files: %v", err)
 	}
-	// name of input file to be "sample.txt"
-	if fileinfo.Name() != "sample.txt" {
-		return "", fmt.Errorf(`wrong input file name, please input "sample.txt" as the input file name`)
-	}
-	// size of the file not to be 0
-	if fileinfo.Size() == 0 {
-		return "", fmt.Errorf("file exists, but is empty. Please ensure sample.txt has text to be formatted")
-	}
-	// open the inputfile and store it in a variable , handle errors
-	inputFile, err := os.Open(inputfilePath)
-	if err != nil {
-		return "", fmt.Errorf("error opening file")
-	}
-	defer inputFile.Close() // saves on memory
 
-	// create output file, handle possible errors
-	// file extension to be .txt
-	if filepath.Ext(outputfilePath) != ".txt" {
-		return "", fmt.Errorf("wrong output file extension, use .txt files only")
-	}
-	// name of file to be result.txt
-	if outputfilePath != "result.txt" {
-		return "", fmt.Errorf(`wrong output file name, please input "result.txt" as the output file name`)
-	}
-	ouputFile, err := os.Create(outputfilePath)
+	// input file open
+	inputfile, err := os.Open(inputfilePath)
 	if err != nil {
-		return "", fmt.Errorf("error creating output file")
+		if os.IsNotExist(err) {
+			return "", fmt.Errorf("error:input/sample file does not exist \n %v", err)
+		} else {
+			return "", fmt.Errorf("error: %v", err)
+		}
 	}
-	defer ouputFile.Close()
+	defer inputfile.Close()
 
-	// read the input file and store the input in a string variable
+	// output file creation
+	outputfile, err := os.Create(outputfilePath)
+	if err != nil {
+		return "", fmt.Errorf("error creating output file: \n%v", err)
+	}
+	defer outputfile.Close()
+
+	// read contents of input file
 	var line string
-	scanner := bufio.NewScanner(inputFile)
+	scanner := bufio.NewScanner(inputfile)
 	for scanner.Scan() {
 		line = scanner.Text()
 	}
